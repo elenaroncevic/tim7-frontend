84b0297f8a3a556dc5ab907383e21afa5a947054
draganaHrcek <dragana.hrcek@gmail.com>
2019-01-25 20:08:23 +0100

    Popunjavanje selekt menija linijama iz aktuelnog cenovnika

--- src/app/get-linije-zone.service.spec.ts
+++ src/app/get-linije-zone.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GetLinijeService } from './get-linije.service';
+import { GetLinijeZoneService } from './get-linije-zone.service';
 
-describe('GetLinijeService', () => {
+describe('GetLinijeZoneService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: GetLinijeService = TestBed.get(GetLinijeService);
+    const service: GetLinijeZoneService = TestBed.get(GetLinijeZoneService);
     expect(service).toBeTruthy();
   });
 });
